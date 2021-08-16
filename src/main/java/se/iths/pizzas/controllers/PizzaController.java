d3231b4b98f357d0e6ef2645ac00b54d393984e0
Martin Blomberg <martin.blomberg@outlook.com>
2021-08-16 11:44:51 +0200

    Add repository to controller

--- src/main/java/se/iths/pizzas/controllers/PizzaController.java
+++ src/main/java/se/iths/pizzas/controllers/PizzaController.java
@@ -3,14 +3,23 @@ package se.iths.pizzas.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.iths.pizzas.entities.Pizza;
+import se.iths.pizzas.repositories.PizzaRepository;
 
 import java.util.List;
 
 @RestController
 public class PizzaController {
 
+    private final PizzaRepository pizzaRepository;
+
+    public PizzaController(PizzaRepository pizzaRepository){
+        this.pizzaRepository = pizzaRepository;
+    }
+
     @GetMapping("/pizzas")
     public List<Pizza> pizzas() {
-        return List.of(new Pizza(1L,"Margerita",50,"ost,tomatsås"));
+        return pizzaRepository.findAll();
     }
+
+
 }
